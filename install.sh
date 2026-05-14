a0cdfeacf59947c503addff1abb101db089ee38b
Felix Li <astralidea@163.com>
2026-05-14 17:43:11 +0800

    Revert "Update EARLYOOM_ARGS in install.sh script"

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ sudo curl -o /opt/starrocks-init/earlyoom "https://raw.githubusercontent.com/Cel
 sudo curl -o /opt/starrocks-init/jmap.sh "https://raw.githubusercontent.com/CelerData/earlyoom-install/main/jmap.sh"
 sudo mv /opt/starrocks-init/earlyoom /usr/bin/earlyoom
 sudo chmod a+x /usr/bin/earlyoom
-sudo sed -i 's/^EARLYOOM_ARGS=.*$/EARLYOOM_ARGS="-M 512000 -m 5 -r 600 -s --ignore-root-user --prefer '\''(^|\/)(starrocks_be|java|agent-service)$'\'' --prefer-only -N \/opt\/starrocks-init\/jmap.sh"/' /etc/default/earlyoom
+sudo sed -i 's/^EARLYOOM_ARGS=.*$/EARLYOOM_ARGS="-M 512000 -m 5 -r 600 --ignore-root-user --prefer '\''(^|\/)(starrocks_be|java|agent-service)$'\'' --prefer-only -N \/opt\/starrocks-init\/jmap.sh"/' /etc/default/earlyoom
 sudo sed -i '/^DynamicUser=/s/^/#/' /lib/systemd/system/earlyoom.service
 sudo sed -i '/^TasksMax=/s/^/#/' /lib/systemd/system/earlyoom.service
 sudo sed -i '/^MemoryMax=/s/^/#/' /lib/systemd/system/earlyoom.service
